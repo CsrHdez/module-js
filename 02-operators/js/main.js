8d792372f1c64a4bd0f23aaa26e5c844e97e2ee1
CsrHdez <devcesar.hdez@gmail.com>
2021-09-01 23:46:55 -0500

    Ejercicio 7 (Tarea)

--- 02-operators/js/main.js
+++ 02-operators/js/main.js
@@ -24,3 +24,15 @@ let char2 = prompt("Escriba otra letra:");
 if ( char1 < char2 || (char1.charCodeAt() == 241 && char2.charCodeAt() >= 79) ) {
 	console.log("La primera letra escrita aparece primero en el abecedario");
 }
+
+console.log("------------------- Ejercicio 7 -------------------");
+let namePerson = prompt('¿Cuál es tu nombre?');
+let weightPerson = prompt('¿Cuál es tu peso actual?');
+const stars = [
+	{name: 'Jupiter', gravity: 24.79},
+	{name: 'Luna', gravity: 1.622},
+	{name: 'Marte', gravity: 3.711}
+];
+const GRAVITY = 9.81;
+
+stars.forEach(star => console.log(`${namePerson} tú peso en ${star.name} es de ${(weightPerson/GRAVITY)*star.gravity}kg`));
