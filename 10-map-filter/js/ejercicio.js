8f6aeedc1122aff8cfc5c6593398288b60460de8
CsrHdez <88517516+CsrHdez@users.noreply.github.com>
2021-09-20 22:22:54 -0500

    Ejercicio en clase 20 Sept

---
+++ 10-map-filter/js/ejercicio.js
@@ -0,0 +1,76 @@
+/* Ejercicio: */
+const songsData = [
+    {
+        name: "¿Dónde jugarán los niños?",
+        band: "Maná",
+        releaseYear: "1992",
+        statistics: {
+            likes: 20000,
+            reproductions: 8000
+        },
+    },
+    {
+        name: "La muralla verde",
+        band: "Enanitos Verdes",
+        releaseYear: "1986",
+        statistics: {
+            likes: 21000,
+            reproductions: 19000
+        },
+    },
+    {
+        name: "Te Ví En Un Tren",
+        band: "Enanitos Verdes",
+        releaseYear: "1987",
+        statistics: {
+            likes: 20000,
+            reproductions: 23490
+        },
+    },
+    {
+        name: "Mariposa Traicionera",
+        band: "Maná",
+        releaseYear: "2002",
+        statistics: {
+            likes: 12000,
+            reproductions: 25690
+        },
+    },
+    {
+        name: "Rayando el Sol",
+        band: "Maná",
+        releaseYear: "1990",
+        statistics: {
+            likes: 12000,
+            reproductions: 18000
+        },
+    },
+    {
+        name: "La celula que explota",
+        band: "Jaguares",
+        releaseYear: "1999",
+        statistics: {
+            likes: 12000,
+            reproductions: 23421
+        },
+    },
+    {
+        name: "No dejes que...",
+        band: "Jaguares",
+        releaseYear: "1999",
+        statistics: {
+            likes: 12345,
+            reproductions: 10000
+        },
+    }
+]
+
+let strings = songs => songs.map(song => `La cancion ${song.name} es de ${song.band}`);
+
+console.log(strings(songsData));
+
+
+let busqueda = (songs, property, wordToSearch) => songs.filter(song => new RegExp(wordToSearch, 'gi').test(song[property]))
+// let busqueda = (songs, property, wordToSearch) => songs.filter(song => song[property].toLowerCase().includes(wordToSearch.toLowerCase()))
+
+console.log(busqueda(songsData, 'name','cel'));
